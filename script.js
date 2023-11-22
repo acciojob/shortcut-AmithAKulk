e30ae13cca1bc6a3e6b2eb4c3862b9faeeaf6fe8
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-22 14:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    // Check if both strings are not empty
+    if (str1.length > 0 && str2.length > 0) {
+        // Use array and join method to concatenate initial letters
+        return [str1.charAt(0).toUpperCase(), str2.charAt(0).toLowerCase()].join('');
+    } else {
+        // Return an empty string if either of the strings is empty
+        return '';
+    }
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Examples
+console.log(shortcut("Amnesty", "International")); // 'AI'
+console.log(shortcut("Hello", "world")); // 'Hw'
+console.log(shortcut("", "International")); // ''
+console.log(shortcut("Amnesty", "")); // ''
